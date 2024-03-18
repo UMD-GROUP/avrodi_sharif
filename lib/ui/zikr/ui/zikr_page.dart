bd8afe4fbfc286dfbda186447d67f97c6641c3c6
ahadjonovss <samandarahadjonov@gmail.com>
2024-03-18 15:53:01 +0500

    fifth day has been done

--- lib/ui/zikr/ui/zikr_page.dart
+++ lib/ui/zikr/ui/zikr_page.dart
@@ -13,6 +13,8 @@ class ZikrPage extends StatelessWidget {
         child: Column(
           children: [
             GlobalAppBar(AppBarType.withSettingsAndPop, title: data.title),
+            Text(
+                "اِذْ هَمَّتْ طَٓائِفَتَانِ مِنْكُمْ اَنْ تَفْشَلَاۙ وَاللّٰهُ وَلِيُّهُمَاۜ وَعَلَى اللّٰهِ فَلْيَتَوَكَّلِ الْمُؤْمِنُونَ   ۝   وَلَقَدْ نَصَرَكُمُ اللّٰهُ بِبَدْرٍ وَاَنْتُمْ اَذِلَّةٌۚ فَاتَّقُوا اللّٰهَ لَعَلَّكُمْ تَشْكُرُونَ   ۝")
           ],
         ),
       ),
